002adc63252b633a4be0c6e2945cb4fc2451e739
MaxRock32 <maxrock32@bk.ru>
2019-02-12 15:30:06 +0300

    Сделал изи и нормал

--- Lesson_7/hw07_easy.py
+++ Lesson_7/hw07_easy.py
@@ -2,6 +2,13 @@
 Задача-1: Реализовать индексацию элементов списка не с нуля, а с 5, т.е. 5, 6, 7
 и т.д.
 '''
+class MyList(list):
+    def __getitem__(self, offset):
+        return list.__getitem__(self, offset - 5)
+
+
+lst = MyList([1, 2, 3, 4, 5])
+print(lst[5])
 
 '''
 Задача-2: Придумать любу структуру данных. Она должна содержать два атрибута.
@@ -13,6 +20,26 @@
 который вы захотите. Проверить еще раз. В комментарии написать, в чем разница
 между подходом с использованием метода __str__ и без него.
 '''
+class MyClass:
+    def __init__(self, number):
+        self.divider = 2
+        self.number = int(number)
+
+    def binary(self):
+        return bin(self.number // self.divider)
+
+    # стандартный вывод отобразит объект с ссылкой на область памяти <__main__.Persimmon object at 0x101b67470>
+    # при использовании перегрузки метода, возможно заменить стандартный вывод на желаемый результат
+    def __str__(self):
+        return f"Результат целочисленного деления числа {self.number} на {self.divider}  - {self.binary()}"
+
+
+a = MyClass(34.45)
+b = a.binary()
+c = str(a)
+
+print(b)
+print(c)
 
 '''
 Задача-3: Продолжить работу над задачей 2. Добавить еще один метод. Он должен
@@ -20,6 +47,27 @@
 Сам метод должен ловить значение и что-то с ним делать и возвращать результат.
 Реализовать для этого метода декоратор @staticmethod
 '''
+class MyClass:
+    def __init__(self, number):
+        self.divider = 2
+        self.number = int(number)
 
+    def binary(self):
+        return bin(self.number // self.divider)
+
+    # стандартный вывод отобразит объект с ссылкой на область памяти <__main__.Persimmon object at 0x101b67470>
+    # при использовании перегрузки метода, возможно заменить стандартный вывод на желаемый результат
+    def __str__(self):
+        return f"Результат целочисленного деления числа {self.number} на {self.divider}  - {self.binary()}"
+
+    @staticmethod
+    def strtobin(string):
+        return string.replace("0b", "")
+
+
+a = MyClass(34.45)
+b = a.binary()
+c = a.strtobin(b)
+print(c)
 
 
