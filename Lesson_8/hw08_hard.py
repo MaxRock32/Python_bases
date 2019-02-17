bb47843c0cc630e9efed3ece5e95a477eda2ff18
MaxRock32 <maxrock32@bk.ru>
2019-02-17 23:08:27 +0300

    сделал дз - 8

--- Lesson_8/hw08_hard.py
+++ Lesson_8/hw08_hard.py
@@ -4,5 +4,29 @@
 Также предусмотрите тот, случай, что пользователь ввел не число, а строку
 (здесь можно применить встроенное исключение).
 '''
+class NegativeNumber(Exception):
+    def __init__(self, number):
+        Exception.__init__(self)
+        self.number = number
 
+    def __str__(self):
+        return f'{self.number} is not positive!'
+
+
+def positive(a):
+    if a <= 0:
+        raise NegativeNumber(a)
+
+
+try:
+    x = int(input("Введите число: "))
+    try:
+        positive(x)
+    except NegativeNumber as ex:
+        x = ex
+
+except ValueError:
+    x = "Это не число"
+
+print(x)
 
